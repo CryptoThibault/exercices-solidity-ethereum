df891d80e75363c7abc4f34be3407464b7d20f69
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-12 15:21:10 +0200

    modif

--- PayableCalculator.sol
+++ PayableCalculator.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.0;
 
 contract PayableCalculator {
-    uint256 private _price = 0.0001 ether;
+    uint256 private _price = 0.001 ether;
     address public _owner;
     uint256 private _gain;
     uint256 public _count;
+
+    event Calc(int256 nb1, int256 nb2, uint256 op, int256 res);
 
     constructor() {
         _owner = msg.sender;
@@ -14,21 +16,23 @@ contract PayableCalculator {
 
     function calc(
         int256 _nb1,
-        uint256 _op,
-        int256 _nb2
-    ) public payable returns (int256) {
+        int256 _nb2,
+        uint256 _op
+    ) public payable returns (int256 _res) {
         require(msg.value == _price, "PayableCalculator: not the exact price");
         require(
             _op != 0 && _op <= 5,
             "PayableCalculator: not a valid opperator"
         );
+        emit Calc(_nb1, _nb2, _op, _res);
         _gain += msg.value;
-        if (_op == 1) return _nb1 + _nb2;
-        else if (_op == 2) return _nb1 - _nb2;
-        else if (_op == 3) return _nb1 * _nb2;
-        else if (_op == 4) return _nb1 / _nb2;
-        else if (_op == 5) return _nb1 % _nb2;
+        if (_op == 1) _res = _nb1 + _nb2;
+        else if (_op == 2) _res = _nb1 - _nb2;
+        else if (_op == 3) _res = _nb1 * _nb2;
+        else if (_op == 4) _res = _nb1 / _nb2;
+        else if (_op == 5) _res = _nb1 % _nb2;
         _count++;
+        return _res;
     }
 
     function cashOut() public {
